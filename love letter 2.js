667d5116c40bcedb47a59fad5942dcf18aa9765f
ttus612 <thanhtu6122002@gmail.com>
2025-02-12 00:11:04 +0700

    commit thu cú

---
+++ love letter 2.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
+$(document).ready(function () {
+  var envelope = $("#envelope-2");
+  var btn_open = $("#open-2");
+  var btn_reset = $("#reset-2");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
+$(document).ready(function () {
+  var envelope = $("#envelope-3");
+  var btn_open = $("#open-3");
+  var btn_reset = $("#reset-3");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
+$(document).ready(function () {
+  var envelope = $("#envelope-4");
+  var btn_open = $("#open-4");
+  var btn_reset = $("#reset-4");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
+$(document).ready(function () {
+  var envelope = $("#envelope-5");
+  var btn_open = $("#open-5");
+  var btn_reset = $("#reset-5");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
